1d6ac7e91c2c82596a1de7b9d4e6cb3bf01c5421
SaarChaffee <saarchaffee@qq.com>
2023-12-07 20:37:10 +0800

    fix: using inject instead using

--- src/index.ts
+++ src/index.ts
@@ -13,53 +13,55 @@ export async function apply(ctx: Context, config: Config = {}): Promise<void> {
 
   ctx.plugin(HitokotoApi, config)
 
-  ctx
-    .command('hitokoto')
-    .alias('一言')
-    .option('type', `-t <type:string>`)
-    .option('min-length', `-l <length:int>`)
-    .option('max-length', `-L <length:int>`)
-    .before(async ({ options, session }) => {
-      if (options?.['min-length'] && options?.['max-length']) {
-        if (options['min-length'] > options['max-length']) {
-          return session?.text('.min_length_gt_max_length')
+  ctx.inject(['hitokoto'], (ctx) => {
+    ctx
+      .command('hitokoto')
+      .alias('一言')
+      .option('type', `-t <type:string>`)
+      .option('min-length', `-l <length:int>`)
+      .option('max-length', `-L <length:int>`)
+      .before(async ({ options, session }) => {
+        if (options?.['min-length'] && options?.['max-length']) {
+          if (options['min-length'] > options['max-length']) {
+            return session?.text('.min_length_gt_max_length')
+          }
         }
-      }
-      if (typeof options?.type === 'undefined') {
-        return
-      }
-      const types = options?.type?.split(',')
-      if (types.length <= 0 || !types.every((t) => t)) {
-        return session?.text('.invalid_type', [options.type])
-      }
-    })
-    .action(async ({ options, session }) => {
-      const params = {
-        c: options?.type?.split(',') ?? config.defaultTypes,
-        min_length: options?.['min-length'] ?? config.minLength,
-        max_length: options?.['max-length'] ?? config.maxLength,
-      }
+        if (typeof options?.type === 'undefined') {
+          return
+        }
+        const types = options?.type?.split(',')
+        if (types.length <= 0 || !types.every((t) => t)) {
+          return session?.text('.invalid_type', [options.type])
+        }
+      })
+      .action(async ({ options, session }) => {
+        const params = {
+          c: options?.type?.split(',') ?? config.defaultTypes,
+          min_length: options?.['min-length'] ?? config.minLength,
+          max_length: options?.['max-length'] ?? config.maxLength,
+        }
 
-      try {
-        const resp = await ctx.hitokoto.getHitokoto(params)
-        return session?.text('.format', resp)
-      } catch (error) {
-        const err = error as Error
-        if (/ETIMEOUT/.test(err.message)) {
-          return session?.text('.timeout')
+        try {
+          const resp = await ctx.hitokoto.getHitokoto(params)
+          return session?.text('.format', resp)
+        } catch (error) {
+          const err = error as Error
+          if (/ETIMEOUT/.test(err.message)) {
+            return session?.text('.timeout')
+          }
+          if (Quester.isAxiosError(error)) {
+            return session?.text('.request_error', [error.status])
+          }
+          return session?.text('.unknown_error', err)
         }
-        if (Quester.isAxiosError(error)) {
-          return session?.text('.request_error', [error.status])
-        }
-        return session?.text('.unknown_error', err)
-      }
-    })
+      })
 
-  ctx.command('hitokoto.types').action(async ({ session }) => {
-    return session?.text('.list', [
-      Object.entries(ctx.hitokoto.types)
-        .map(([type, desc]) => `${type} - ${desc}`)
-        .join('\n'),
-    ])
+    ctx.command('hitokoto.types').action(async ({ session }) => {
+      return session?.text('.list', [
+        Object.entries(ctx.hitokoto.types)
+          .map(([type, desc]) => `${type} - ${desc}`)
+          .join('\n'),
+      ])
+    })
   })
 }
